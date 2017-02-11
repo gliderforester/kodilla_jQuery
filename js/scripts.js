d70ea2b501ab1490b539777dc99775e57303c829
gliderforester <gliderforester@o2.pl>
2017-02-11 21:05:32 +0100

    Korzystamy z jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,21 @@
 $(function(){
+/* $('span').each(function(index, element) {
+	if(index % 2 == 0) {
+		$(element).css('color', 'red');
+	}
+});*/
 
+ $('span:odd').css('color', 'red');
+
+
+	$('p').each(function(index, element) {
+		var button = '<button class="btn" data-tmp="' + index + '"> Naciśnij </button>'
+		$(element).append(button);
+	});
+
+	$('button').click(function(){
+		alert($(this).attr("data-tmp"));
+	})
 
 
 });
