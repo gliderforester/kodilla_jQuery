27cc057382709bb8820c05d1053a1fe037fb0959
gliderforester <gliderforester@o2.pl>
2017-02-14 17:48:15 +0100

    carousel

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
-$(function(){
+$(function () {
+	var carouselList = $("#carousel ul");
+	setInterval(changeSlide, 4000);
 
-
-
+	function changeSlide() {
+		carouselList.animate({'marginLeft': -400}, 2000, moveFirstSlide);
+	};
+	function moveFirstSlide() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft:0});
+	}
 });
